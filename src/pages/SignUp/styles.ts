aa1797923e7e9139d07dfb48d183553f7031d572
Matheus Padilha <mvpadilha94@gmail.com>
2020-08-02 18:39:10 -0300

    first commit

---
+++ src/pages/SignUp/styles.ts
@@ -0,0 +1,93 @@
+import styled, { keyframes } from 'styled-components';
+import { shade } from 'polished';
+
+import signUnBackgroundImg from '../../assets/sign-up-background.png';
+
+export const Container = styled.div`
+  /* ocupa a altura total da tela */
+  height: 100vh;
+
+  display: flex;
+
+  /* estica o maximo que poder */
+  align-items: stretch;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  width: 100%;
+  max-width: 700px;
+`;
+
+// animation
+const appearFromright = keyframes`
+  from {
+    opacity: 0;
+    transform: translateX(50px);
+  }
+
+  to {
+    opacity: 1;
+    transform: translateX(0);
+  }
+`;
+
+export const AnimationContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  animation: ${appearFromright} 1s;
+
+  form {
+    margin: 80px 0;
+    width: 340px;
+    text-align: center;
+
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    a {
+      color: #f4ede8;
+      display: block;
+      margin-top: 24px;
+      text-decoration: none;
+      transition: color 0.2s;
+
+      &:hover {
+        color: ${shade(0.2, '#f4ede8')};
+      }
+    }
+  }
+  /* > informa que sera aplicado apenas aos que estao diretamente dentro do content  */
+  > a {
+    color: #f4ede8;
+    display: block;
+    margin-top: 24px;
+    text-decoration: none;
+    transition: color 0.2s;
+
+    display: flex;
+    align-items: center;
+
+    svg {
+      margin-right: 16px;
+    }
+
+    &:hover {
+      color: ${shade(0.2, '#f4ede8')};
+    }
+  }
+`;
+
+export const Background = styled.div`
+  flex: 1;
+  background: url(${signUnBackgroundImg}) no-repeat center;
+  background-size: cover;
+`;
